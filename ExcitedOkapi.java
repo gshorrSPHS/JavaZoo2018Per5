0042757391dfbf9fc9d696c83222d8c233bb9be6
Amurdock2 <37672688+Amurdock2@users.noreply.github.com>
2018-03-22 13:49:42 -0700

    Aaron's Animal

---
+++ ExcitedOkapi.java
@@ -0,0 +1,13 @@
+public class ExcitedOkapi extends Okapi
+{
+    public ExcitedOkapi()
+    {
+        super(7);
+    }
+    
+    @Override
+    public String run()
+    {
+        return "First it reached speeds deemed impossible. Then it got faster.";
+    }
+}
